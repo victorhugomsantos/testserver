8b242f0ab97ee76c1768ea7a99bf692446ed3d71
Victor Santos <vichugomsantos@gmail.com>
2024-02-15 12:40:19 -0300

    "syncando"

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!!");
+const express = require('express');
+const { request } = require('http');
+
+const app = express();
+
+app.get('/',(request,response)=>{
+        return response.json({message: 'serveris up'});
+
+})
+
+app.listen(3333)
